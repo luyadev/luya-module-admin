3d3f7bca9ff23a3625c290495a44241e7f122d53
Basil Suter <git@nadar.io>
2017-05-09 21:11:55 +0200

    Moved Active Window scaffolding command into admin Module. closes #1294

--- src/commands/views/aw/create.php
+++ src/commands/views/aw/create.php
@@ -1,47 +1,51 @@
 <?php
+/**
+ * @var $className
+ * @var $namespace
+ * @var $luya
+ * @var $moduleId
+ * @var $alias
+ */
 
-namespace luya\admin\aws;
+echo "<?php\n";
+?>
+
+namespace <?= $namespace; ?>;
 
 use Yii;
 use luya\admin\ngrest\base\ActiveWindow;
 
 /**
- * Storage Effect Active Window.
+ * <?= $alias; ?>.
+ *
+ * <?= $luyaText; ?> 
  */
-final class StorageFilterImagesActiveWindow extends ActiveWindow
+class <?= $className; ?> extends ActiveWindow
 {
     /**
      * @var string The name of the module where the ActiveWindow is located in order to finde the view path.
      */
-    public $module = '@admin';
-    
+    public $module = '@<?= $moduleId; ?>';
+	
     /**
      * @var string The name of of the ActiveWindow. This is displayed in the CRUD list.
      */
-    public $alias = 'Images';
-    
+    public $alias = '<?= $alias; ?>';
+	
     /**
      * @var string The icon name from goolges material icon set (https://material.io/icons/)
      */
-    public $icon = 'filter_vintage';
-    
+    public $icon = 'extension';
+	
     /**
      * The default action which is going to be requested when clicking the ActiveWindow.
-     *
+     * 
      * @return string The response string, render and displayed trough the angular ajax request.
      */
     public function index()
     {
         return $this->render('index', [
-            'model' => $this->model,
-            'images' => Yii::$app->storage->findImages(['filter_id' => $this->model->id]),
+            'id' => $this->itemId,
         ]);
-    }
-    
-    public function callbackRemove()
-    {
-        $this->model->removeImageSources();
-        
-        return $this->sendSuccess("Removed image filter {this->model->name} versions.");
     }
 }
