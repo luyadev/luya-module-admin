8dec22cbf45a9127727f551963538723e2a44f35
baqianxin <baqianxin@readtv.cn>
2018-01-09 18:00:59 +0800

    the echarts of dashboardObject

--- src/assets/Main.php
+++ src/assets/Main.php
@@ -27,6 +27,7 @@ class Main extends \luya\web\Asset
      */
     public $js = [
         'dist/js/main.js',
+        'dist/js/echarts.min.js',
     ];
 
     /**
@@ -35,7 +36,7 @@ class Main extends \luya\web\Asset
     public $depends = [
         'luya\admin\assets\Jquery',
     ];
-    
+
     /**
      * @inheritdoc
      */
---
+++ src/dashboard/ChartDashboardObject.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace luya\admin\dashboard;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Fast generated Dashboard Objects.
+ *
+ * The default object is the default class for all {{luya\admin\base\Module::$dashboardObjects}} items without a class defintion.
+ *
+ * @author Oom <baqianxin@163.com>
+ * @blog  http://oom-cc.com
+ * @since 1.0.0
+ */
+class ChartDashboardObject extends BaseDashboardObject {
+
+
+    public $wrapperOptions = ['class' => 'card', 'tag' => 'div'];
+
+    /**
+     * @var string The Chart's option
+     *
+     */
+    public $options = '';
+
+    public function getOuterTemplate(){
+        $uniqid = md5(uniqid(microtime(true),true));
+        return '<div class="card-header"><h4>{{title}}<h4></div><echarts id="charts_'.$uniqid.'" legend="legend" item="item" data="data"></echarts>';
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getOuterTemplateContent()
+    {
+        $options = ArrayHelper::merge([
+            'ng-controller' => 'DefaultDashboardObjectController',
+            'ng-init' => 'loadData(\'{{dataApiUrl}}\')'], $this->wrapperOptions);
+
+        return Html::tag(ArrayHelper::remove($this->wrapperOptions, 'tag', 'div'), $this->getOuterTemplate(), $options);
+    }
+}
