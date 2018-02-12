36b0dcdb38484a71f3abd215d3959da089b38314
Basil Suter <git@nadar.io>
2018-02-12 11:17:09 +0400

    add margin bottom for dashboard objects

--- src/views/default/dashboard.php
+++ src/views/default/dashboard.php
@@ -1,7 +1,7 @@
 <div class="luya-content">
     <div class="row">
     <?php foreach ($items as $dashboard): /* @var $dashboard \luya\admin\base\DashboardObjectInterface */ ?>
-    <div class="col-md-4">
+    <div class="col-md-4 mb-2">
     	<?= $dashboard->getTemplate(); ?>
     </div>
     <?php endforeach; ?>
