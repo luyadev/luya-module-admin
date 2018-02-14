2e69a1150adf676b4b6ca60383a3417985d0558c
Basil Suter <git@nadar.io>
2018-02-14 13:48:25 +0400

    improve admin url rule php doc

--- src/components/UrlRule.php
+++ src/components/UrlRule.php
@@ -5,31 +5,45 @@ namespace luya\admin\components;
 use yii;
 
 /**
- * Url rule for NgRest Apis
+ * Url rule for NgRest Apis.
+ * 
+ * As ngrest apis should have the same module as identifer like `admin/api-user-lists` therefore
+ * this rule is going to "fake" those admin module routes.
+ * 
+ * The controllerMap property from the admin module is provided by {{luya\web\Bootstrap::run()}} method.
  *
  * @author Basil Suter <basil@nadar.io>
  * @since 1.0.0
  */
 class UrlRule extends \yii\rest\UrlRule
 {
+	/**
+	 * @inheritdoc
+	 */
+	public $pluralize = false;
+	
     /**
      * @inheritdoc
      */
     public function init()
     {
+    	// get all controller mappings
+    	// the key is the alias the value the path to the api class.
         $map = Yii::$app->getModule('admin')->controllerMap;
 
+        // if there are rules we have to set them into the url rules controller property.
         if (count($map) > 0) {
+        	// assign the url rule from the admins controller map
             foreach ($map as $alias => $className) {
-                $class = sprintf('%s/%s', 'admin', $alias);
-                $this->controller[] = $class;
+                $this->controller[] = sprintf('%s/%s', 'admin', $alias);
             }
 
-            parent::init();
-        } else {
-            $this->controller = [];
+            return parent::init();
         }
+        
+        // if there is no controller from controller map we have to fake
+        // an empty controller property otherwise the url rule will generate
+        // an error.
+        $this->controller = [];
     }
-    
-    public $pluralize = false;
 }
