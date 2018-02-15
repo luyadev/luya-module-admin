db3c7883d9048760eca4fbb48a4da3f0b8362c09
Basil Suter <git@nadar.io>
2018-02-15 13:01:40 +0100

    add is delete event for ngrest log #59

--- src/models/NgrestLog.php
+++ src/models/NgrestLog.php
@@ -15,6 +15,7 @@ use luya\helpers\Json;
  * @property string $api
  * @property int $is_update
  * @property int $is_insert
+ * @property int $is_delete
  * @property string $attributes_json
  * @property string $attributes_diff_json
  * @property string $pk_value
@@ -48,28 +49,50 @@ class NgrestLog extends \yii\db\ActiveRecord
 	{
 		return [
 			[['user_id', 'timestamp_create', 'route', 'api', 'attributes_json'], 'required'],
-			[['user_id', 'timestamp_create', 'is_update', 'is_insert'], 'integer'],
+			[['user_id', 'timestamp_create', 'is_update', 'is_insert', 'is_delete'], 'integer'],
 			[['attributes_json', 'attributes_diff_json'], 'string'],
 			[['route', 'api'], 'string', 'max' => 80],
 			[['pk_value', 'table_name'], 'string', 'max' => 255],
 		];
 	}
 	
+	/**
+	 * Get attributes json as array.
+	 * 
+	 * @return array
+	 */
 	public function getAttributesJsonArray()
 	{
 		return $this->convertValueToJson($this->attributes_json);
 	}
 	
+	/**
+	 * Get attributes json diff as array.
+	 * 
+	 * @return array
+	 */
 	public function getAttributesJsonDiffArray()
 	{
 		return $this->convertValueToJson($this->attributes_diff_json);
 	}
 	
+	/**
+	 * Get a given attribute by its name from the attributes json diff array.
+	 * 
+	 * @param string $attribute The attribute to check inside the array.
+	 * @return mixed
+	 */
 	public function getAttributeFromJsonDiffArray($attribute)
 	{
 		return isset($this->getAttributesJsonDiffArray()[$attribute]) ? $this->getAttributesJsonDiffArray()[$attribute] : '';
 	}
 	
+	/**
+	 * Convert a given value into an array.
+	 * 
+	 * @param string $value
+	 * @return array
+	 */
 	protected function convertValueToJson($value)
 	{
 		if (Json::isJson($value)) {
@@ -92,6 +115,7 @@ class NgrestLog extends \yii\db\ActiveRecord
 			'api' => 'Api',
 			'is_update' => 'Is Update',
 			'is_insert' => 'Is Insert',
+			'is_delete' => 'Is Delete',
 			'attributes_json' => 'Attributes Json',
 			'attributes_diff_json' => 'Attributes Diff Json',
 			'pk_value' => 'Pk Value',
