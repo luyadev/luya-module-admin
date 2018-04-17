54fde26f481d12c33f44bb078732d97a04c855fa
Marc Stampfli <marc.stampfli@zephir.ch>
2018-04-17 18:03:20 +0200

    Enabled as many files as possible for uglification / minifcation

--- src/resources/gulp-config.js
+++ src/resources/gulp-config.js
@@ -101,7 +101,12 @@ module.exports = {
         },
         js: {
             "dist/js/main.js": [
-                "../../vendor/bower-asset/angular/angular.min.js",
+                "../../vendor/bower-asset/angular/angular.js",
+                "vendorlibs/ng-colorwheel/ng-colorwheel.js",
+            ]
+        },
+        jsUglify: {
+            "dist/js/mainugly.js": [
                 "../../vendor/bower-asset/angular-loading-bar/build/loading-bar.min.js",
                 "../../vendor/bower-asset/angularjs-datepicker/dist/angular-datepicker.min.js",
                 "../../vendor/bower-asset/ui-router/release/angular-ui-router.min.js",
@@ -111,14 +116,11 @@ module.exports = {
                 "../../vendor/bower-asset/ng-wig/dist/ng-wig.min.js",
                 "../../vendor/bower-asset/twigjs-bower/twig/twig.js",
                 "../../vendor/bower-asset/angular-filter/dist/angular-filter.min.js",
-                "vendorlibs/ng-colorwheel/ng-colorwheel.js", // Don't minify
+                // Colorwheel?
                 "../../vendor/bower-asset/bowser/src/useragent.js",
                 "../../vendor/bower-asset/bowser/src/bowser.js",
                 "../../vendor/bower-asset/echarts/dist/echarts.min.js",
-            ]
-        },
-        jsUglify: {
-            "dist/js/mainugly.js": [
+
                 "js/dnd.js",
                 "js/zaa.js",
                 "js/services.js",
