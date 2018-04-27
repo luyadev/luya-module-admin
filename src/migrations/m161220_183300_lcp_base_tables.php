b3666d8147a54a1513af22098c73465543d3fa52
Carsten Brandt <mail@cebe.cc>
2018-04-27 15:56:22 +0200

    Update m161220_183300_lcp_base_tables.php
    fixes #109

--- src/migrations/m161220_183300_lcp_base_tables.php
+++ src/migrations/m161220_183300_lcp_base_tables.php
@@ -10,7 +10,8 @@ class m161220_183300_lcp_base_tables extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string(255)->notNull(),
             'access_token' => $this->string(255)->notNull(),
-            'identifier' => $this->string(255)->notNull()->unique(),
+            // max 190 chars to fit the length limit for indexes on MySQL with utf8mb4 encoding
+            'identifier' => $this->string(190)->notNull()->unique(),
             'is_deleted' => $this->boolean()->defaultValue(false),
             'is_disabled' => $this->boolean()->defaultValue(false),
         ]);
@@ -19,7 +20,8 @@ class m161220_183300_lcp_base_tables extends Migration
             'id' => $this->primaryKey(),
             'machine_id' => $this->integer(11)->notNull(),
             'timestamp' => $this->integer(11)->notNull(),
-            'build_token' => $this->string(255)->notNull()->unique(),
+            // max 190 chars to fit the length limit for indexes on MySQL with utf8mb4 encoding
+            'build_token' => $this->string(190)->notNull()->unique(),
             'config' => $this->text()->notNull(),
             'is_complet' => $this->boolean()->defaultValue(false),
             'expiration_time' => $this->integer(11)->notNull(),
