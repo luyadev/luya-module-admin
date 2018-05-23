bfc0511efb921ba7fc0e57d5089b6b48746e9583
Basil <git@nadar.io>
2018-05-23 15:51:02 +0200

    add stroke offset calculation #128 (#130)

--- src/apis/TimestampController.php
+++ src/apis/TimestampController.php
@@ -5,7 +5,6 @@ namespace luya\admin\apis;
 use Yii;
 use luya\admin\models\UserOnline;
 use luya\admin\base\RestController;
-use luya\admin\models\Config;
 
 /**
  * Timestamp API, refreshes the UserOnline system of the administration area.
@@ -22,17 +21,19 @@ class TimestampController extends RestController
      */
     public function actionIndex()
     {
+        $userId = Yii::$app->adminuser->id;
         // clear user online list
         UserOnline::clearList($this->module->userIdleTimeout);
+        $userOnlineModel = UserOnline::findOne(['user_id' => Yii::$app->adminuser->id]);
         
-        if (!UserOnline::findOne(['user_id' => Yii::$app->adminuser->id])) {
+        if (!$userOnlineModel) {
             Yii::$app->response->statusCode = 401;
             return Yii::$app->response->send();
         }
-        
-        $forceReload = Yii::$app->adminuser->identity->force_reload;
                 
         // if developer is enabled, check if vendor has changed and run the required commands and force reload
+        // @TODO: its a concept
+        /*
         if (!YII_ENV_PROD) {
             $config = (int) Config::get(Config::CONFIG_INSTALLER_VENDOR_TIMESTAMP, null);
             $ts = Yii::$app->packageInstaller->timestamp;
@@ -40,12 +41,24 @@ class TimestampController extends RestController
                 // run migration and import process for developer usage.
             }
         }
+        */
+        
+        // get the stroke-dashoffset for the given user, this indicates the time he is idling
+        // stroke-dashoffset="88px" = 0 // which means 0 percent of time has elapsed
+        // stroke-dashoffset="0px" => 100 // which means 100 percent of time has elpased, auto logout will redirect the user
+        $seconds = (time() - $userOnlineModel->last_timestamp);
+        $percentage = round(($seconds / $this->module->userIdleTimeout) * 100);
+        $offsetPercent = round((82/100) * $percentage);
+        $strokeOffset = 82 - $offsetPercent;
         
         // return users, verify force reload.
         $data = [
+            'idleSeconds' => $seconds,
+            'idlePercentage' => $percentage,
+            'idleStrokeDashoffset' => $strokeOffset,
             'useronline' => UserOnline::getList(),
-            'forceReload' => $forceReload,
-            'locked' => UserOnline::find()->select(['lock_pk', 'lock_table', 'last_timestamp', 'u.firstname', 'u.lastname', 'u.id'])->where(['!=', 'u.id', Yii::$app->adminuser->id])->joinWith('user as u')->createCommand()->queryAll(),
+            'forceReload' => Yii::$app->adminuser->identity->force_reload,
+            'locked' => UserOnline::find()->select(['lock_pk', 'lock_table', 'last_timestamp', 'u.firstname', 'u.lastname', 'u.id'])->where(['!=', 'u.id', $userId])->joinWith('user as u')->createCommand()->queryAll(),
         ];
         
         return $data;
