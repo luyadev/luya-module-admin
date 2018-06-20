3e50675454a5adca6dc6e45d3e79e235cf5fb503
Basil Suter <git@nadar.io>
2018-06-20 15:40:36 +0200

    fix directives

--- src/views/layouts/_angulardirectives.php
+++ src/views/layouts/_angulardirectives.php
@@ -127,9 +127,9 @@ use luya\admin\helpers\Angular;
         <?php if (Yii::$app->adminuser->canRoute('admin/storage/index')): ?>
         <div class="link-selector-actions">
             <div class="link-selector-btn btn btn-secondary" ng-click="toggleModal()">
-                <i class="material-icons left" ng-show="!fileinfo.name">file_upload</i>
-                <i class="material-icons left" ng-show="fileinfo.name">attach_file</i>
-                <span ng-if="fileinfo.name">{{fileinfo.name_original | truncateMiddle: 30}}</span>
+                <i class="material-icons left" ng-show="!fileinfo.name_original">file_upload</i>
+                <i class="material-icons left" ng-show="fileinfo.name_original">attach_file</i>
+                <span ng-if="fileinfo.name_original">{{fileinfo.name_original | truncateMiddle: 30}}</span>
                 <span ng-if="!fileinfo.name_original">
                     <?= Admin::t('layout_select_file'); ?>
                 </span>
@@ -159,7 +159,7 @@ use luya\admin\helpers\Angular;
             <div class="imageupload-preview-sizer"></div>
             <img ng-src="{{thumb.source}}" ng-show="imageinfo != null" class="imageupload-preview-image" />
             <div class="imageupload-infos">
-                <div class="imageupload-size" ng-show="!imageLoading">{{ imageinfo.resolutionWidth }} x {{ imageinfo.resolutionHeight }}</div>
+                <div class="imageupload-size" ng-show="!imageLoading">{{ imageinfo.resolution_width }} x {{ imageinfo.resolution_height }}</div>
             </div>
         </div>
         
