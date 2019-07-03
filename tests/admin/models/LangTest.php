f03630f145d500b72402656859dd49b2a7dc6131
Basil Suter <git@nadar.io>
2019-07-03 10:31:29 +0200

    add language test with flush

---
+++ tests/admin/models/LangTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace luya\admin\tests\admin\models;
+
+use admintests\AdminModelTestCase;
+use luya\testsuite\fixtures\NgRestModelFixture;
+use luya\admin\models\Lang;
+use luya\admin\models\NgrestLog;
+
+class LangTest extends AdminModelTestCase
+{
+    public function testEvents()
+    {
+        $fixture = new NgRestModelFixture([
+            'modelClass' => Lang::class,
+        ]);
+
+        $log = new NgRestModelFixture([
+            'modelClass' => NgrestLog::class,
+        ]);
+
+        /** @var Lang $model */
+        $model = $fixture->newModel;
+        $model->short_code = 'fr';
+        $model->name = 'Francais';
+        $model->is_default = 1;
+        $model->is_deleted = 0;
+
+        $this->assertTrue($model->validate());
+        $this->assertTrue($model->insert());
+    }
+}
