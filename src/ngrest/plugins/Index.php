2781cd5b635ef05d41bf86b05af70085a14605d5
Basil <git@nadar.io>
2019-10-09 18:19:11 +0200

    add debug info

--- src/ngrest/plugins/Index.php
+++ src/ngrest/plugins/Index.php
@@ -4,5 +4,11 @@ namespace luya\admin\ngrest\plugins;
 
 class Index extends Angular
 {
+    /**
+     * config.groupBy
+     * config.groupByExpanded
+     * 
+     * contains group data.
+     */
     public $template = '{{1+((pager.currentPage-1) * pager.perPage)+$index}}';
 }
