eb09aeccb5fc8b4b057c8267c8b43f1f74fe56db
Basil Suter <git@nadar.io>
2019-11-01 11:30:56 +0100

    add unit test

--- tests/admin/apis/StorageControllerTest.php
+++ tests/admin/apis/StorageControllerTest.php
@@ -4,6 +4,8 @@ namespace luya\admin\tests\admin\apis;
 
 use admintests\AdminModelTestCase;
 use luya\admin\apis\StorageController;
+use luya\admin\models\StorageFile;
+use luya\testsuite\fixtures\NgRestModelFixture;
 use luya\testsuite\scopes\PermissionScope;
 
 class StorageControllerTest extends AdminModelTestCase
@@ -28,6 +30,26 @@ class StorageControllerTest extends AdminModelTestCase
             $scope->createRoute(StorageController::PERMISSION_ROUTE);
             $this->expectException('yii\web\ForbiddenHttpException');
             $scope->runControllerAction($ctrl, 'images-upload');
+        });
+    }
+
+    public function testPermissionLessDataAction()
+    {
+        
+
+        $ctrl = new StorageController('id', $this->app);
+
+        PermissionScope::run($this->app, function(PermissionScope $scope) use ($ctrl) {
+            $fixture = new NgRestModelFixture([
+                'modelClass' => StorageFile::class,
+            ]);
+            $data = $scope->runControllerAction($ctrl, 'data-files');
+            $this->assertSame([], $data);
+            $fixture->cleanup();
+        }, function(PermissionScope $config) {
+            $config->userFixtureData = [
+                'is_api_user' => 0,
+            ];
         });
     }
 }
