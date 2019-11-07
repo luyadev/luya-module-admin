c6231303cc26542f8c4212c6c626d34eaa3ce38f
Basil Suter <git@nadar.io>
2019-11-07 16:03:35 +0100

    add changelog and "test"

---
+++ tests/admin/ngrest/plugins/RawTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace admintests\admin\ngrest\plugins;
+
+use admintests\AdminTestCase;
+use luya\admin\ngrest\plugins\Raw;
+
+class RawTest extends AdminTestCase
+{
+    public function testHtmlNewLine()
+    {
+        $raw = new Raw(['name' => 'raw', 'alias' => 'raw', 'i18n' => false]);
+        $this->assertSame('<div ng-bind-html="model | trustAsUnsafe"></div>', $raw->renderList('id', 'model'));
+        $this->assertSame('<zaa-textarea fieldid="id" model="model" label="raw" fieldname="raw" i18n=""></zaa-textarea>', $raw->renderCreate('id', 'model'));
+    }
+}
