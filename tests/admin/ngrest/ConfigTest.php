1e1f27f92749a4c293576eab9b58fe00cf7f5d3f
Basil <git@nadar.io>
2019-12-13 09:12:22 +0100

    add tests

--- tests/admin/ngrest/ConfigTest.php
+++ tests/admin/ngrest/ConfigTest.php
@@ -2,10 +2,15 @@
 
 namespace admintests\admin\ngrest;
 
-use admintests\AdminTestCase;
+use admintests\AdminModelTestCase;
+use luya\admin\buttons\TimestampActiveButton;
+use luya\admin\models\NgrestLog;
+use luya\admin\models\User;
+use luya\admin\models\UserLogin;
 use luya\admin\ngrest\Config;
+use luya\testsuite\fixtures\NgRestModelFixture;
 
-class ConfigTest extends AdminTestCase
+class ConfigTest extends AdminModelTestCase
 {
     /**
      * @expectedException yii\base\InvalidConfigException
@@ -22,5 +27,43 @@ class ConfigTest extends AdminTestCase
         $cfg = new Config(['apiEndpoint' => 'rest-url', 'primaryKey' => ['id']]);
         $this->assertEquals(true, $cfg->addField('list', 'foo'));
         $this->assertEquals(false, $cfg->addField('list', 'foo'));
+    }
+    
+    public function testLazyLoadGetters()
+    {
+        $cfg = new Config(['apiEndpoint' => 'rest-url', 'primaryKey' => ['id']]);
+
+        $fixture = new NgRestModelFixture([
+            'modelClass' => StubUserModel::class,
+        ]);
+
+        $cfg->setModel($fixture->newModel);
+        
+        $this->assertSame([], $cfg->getRelations());
+        $this->assertSame([
+            [
+                'class' => TimestampActiveButton::class,
+                'attribute' => 'foo',
+            ]
+        ], $cfg->getActiveButtons());
+        
+
+    }
+}
+
+class StubUserModel extends User
+{
+    public function ngRestActiveButtons()
+    {
+        return [
+            ['class' => TimestampActiveButton::class, 'attribute' => 'foo']
+        ];
+    }
+
+    public function ngRestRelations()
+    {
+        return [
+            //['label' => 'Label', 'targetModel' => NgrestLog::class, 'dataProvider' => $this->getNgrestLogs()],
+        ];
     }
 }
