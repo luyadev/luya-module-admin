e68ec7d9b4696cbad7eb1f894e3f4a53227e4514
Basil Suter <git@nadar.io>
2020-01-06 11:22:34 +0100

    add exception tests

--- tests/admin/ngrest/plugins/BadgeTest.php
+++ tests/admin/ngrest/plugins/BadgeTest.php
@@ -16,4 +16,20 @@ class BadgeTest extends AdminTestCase
         $raw = new Badge(['name' => 'badge', 'alias' => 'raw', 'i18n' => false, 'variations' => [1 => 'warning'], 'pill' => true]);
         $this->assertSame('<div><span ng-switch="model"><span ng-switch-when="1" class="badge badge-pill badge-warning" ng-bind="model"></span><span ng-switch-default class="badge badge-pill badge-secondary" ng-bind="model"></span></span></div>', $raw->renderList('id', 'model'));
     }
+
+    public function testCreate()
+    {
+        $raw = new Badge(['name' => 'badge', 'alias' => 'raw', 'i18n' => false, 'variations' => [1 => 'warning']]);
+
+        $this->expectException('luya\Exception');
+        $raw->renderCreate('id', 'model');
+    }
+
+    public function testupdate()
+    {
+        $raw = new Badge(['name' => 'badge', 'alias' => 'raw', 'i18n' => false, 'variations' => [1 => 'warning']]);
+
+        $this->expectException('luya\Exception');
+        $raw->renderUpdate('id', 'model');
+    }
 }
