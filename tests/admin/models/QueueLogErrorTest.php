d8574591ec9ea2c65ddc5b83619c4e4e08102029
Basil Suter <git@nadar.io>
2020-02-05 16:07:47 +0100

    test

--- tests/admin/models/QueueLogErrorTest.php
+++ tests/admin/models/QueueLogErrorTest.php
@@ -5,13 +5,16 @@ namespace luya\admin\tests\admin\models;
 use admintests\AdminModelTestCase;
 use luya\admin\models\QueueLogError;
 use luya\testsuite\fixtures\NgRestModelFixture;
+use luya\testsuite\traits\AdminDatabaseTableTrait;
 
 class QueueLogErrorTest extends AdminModelTestCase
 {
+    use AdminDatabaseTableTrait;
+
     public function testModel()
     {
         $fixture = new NgRestModelFixture(['modelClass' => QueueLogError::class]);
-
+        $this->createAdminNgRestLogFixture();
         $this->assertTrue(is_array($fixture->newModel->attributeLabels()));
 
         $model = $fixture->newModel;
