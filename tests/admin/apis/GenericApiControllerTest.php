7577184ed01ee83b3ec0fc5ca813d64eedf118e2
Basil Suter <git@nadar.io>
2020-04-28 14:23:07 +0200

    generic tests

---
+++ tests/admin/apis/GenericApiControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace admintests\admin\controllers;
+
+use admintests\AdminModelTestCase;
+use luya\admin\apis\ApiUserController;
+use luya\admin\apis\EffectController;
+use luya\admin\apis\FilterController;
+use luya\admin\apis\GroupController;
+use luya\admin\apis\LangController;
+use luya\admin\apis\LoggerController;
+use luya\admin\apis\NgrestLogController;
+use luya\admin\apis\ProxyBuildController;
+use luya\admin\apis\ProxyMachineController;
+use luya\admin\apis\QueueLogController;
+use luya\admin\apis\QueueLogErrorController;
+use luya\admin\apis\TagController;
+use luya\admin\apis\UserController;
+use luya\testsuite\fixtures\NgRestModelFixture;
+use Yii;
+use yii\db\ActiveQuery;
+
+class GenericApiControllerTest extends AdminModelTestCase
+{
+    public $controllers = [
+        NgrestLogController::class,
+        ApiUserController::class,
+        EffectController::class,
+        FilterController::class,
+        GroupController::class,
+        LangController::class,
+        LoggerController::class,
+        ProxyBuildController::class,
+        ProxyMachineController::class,
+        QueueLogController::class,
+        QueueLogErrorController::class,
+        TagController::class,
+        UserController::class,
+    ];
+
+    public function testControllerGenericMethodsForCoverage()
+    {
+        $this->createAdminLangFixture();
+        $this->createAdminUserAuthNotificationTable();
+        foreach ($this->controllers as $ctrl) {
+
+            $ctrl = Yii::createObject(['class' => $ctrl], ['id', $this->app]);
+
+            new NgRestModelFixture([
+                'modelClass' => $ctrl->modelClass,
+            ]);
+
+            $this->assertInstanceOf(ActiveQuery::class, $ctrl->prepareListQuery());
+        }
+    }
+}
---
+++ tests/admin/controllers/GenericNgRestControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace admintests\admin\controllers;
+
+use admintests\AdminModelTestCase;
+use luya\admin\controllers\ApiUserController;
+use luya\admin\controllers\EffectController;
+use luya\admin\controllers\FilterController;
+use luya\admin\controllers\GroupController;
+use luya\admin\controllers\LangController;
+use luya\admin\controllers\LoggerController;
+use luya\admin\controllers\NgrestLogController;
+use luya\admin\controllers\ProxyBuildController;
+use luya\admin\controllers\ProxyMachineController;
+use luya\admin\controllers\QueueLogController;
+use luya\admin\controllers\QueueLogErrorController;
+use luya\admin\controllers\TagController;
+use luya\admin\controllers\UserController;
+use Yii;
+
+class GenericNgRestControllerTest extends AdminModelTestCase
+{
+    public $controllers = [
+        NgrestLogController::class,
+        ApiUserController::class,
+        EffectController::class,
+        FilterController::class,
+        GroupController::class,
+        LangController::class,
+        LoggerController::class,
+        ProxyBuildController::class,
+        ProxyMachineController::class,
+        QueueLogController::class,
+        QueueLogErrorController::class,
+        TagController::class,
+        UserController::class,
+    ];
+
+    public function testControllerGenericMethodsForCoverage()
+    {
+        $this->createAdminLangFixture();
+
+        foreach ($this->controllers as $ctrl) {
+
+            $ctrl = Yii::createObject(['class' => $ctrl], ['id', $this->app]);
+
+            $ctrl->setDescription('foo');
+            $this->assertNotNull($ctrl->getDescription()); 
+        }
+    }
+}
