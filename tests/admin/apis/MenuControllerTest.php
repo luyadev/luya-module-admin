ec99bb82d79407fcf9b7a0d260de61ef926c27cf
Basil Suter <git@nadar.io>
2020-04-29 12:14:18 +0200

    add menu data for tests

--- tests/admin/apis/MenuControllerTest.php
+++ tests/admin/apis/MenuControllerTest.php
@@ -13,17 +13,22 @@ class MenuControllerTest extends AdminModelTestCase
 
     public function testLoadDataWithoutApiUser()
     {
+        $this->app->getModule('admin')->moduleMenus = ['admin' => $this->app->getModule('admin')->getMenu()];
         PermissionScope::run($this->app, function (PermissionScope $scope) {
             $this->createAdminLangFixture([]);
-            $scope->createAndAllowRoute('adminmodeltest/id/dashboard');
-            $ctrl = new MenuController('id', $this->app);
+            $scope->createAndAllowRoute('admin/id/dashboard');
+            $ctrl = new MenuController('id', $this->app->getModule('admin'));
             $response = $scope->runControllerAction($ctrl, 'dashboard', ['nodeId' => 1]);
             $this->assertSame([], $response);
+
+            $this->assertSame([], $ctrl->actionIndex());
+            $this->assertSame([], $ctrl->actionItems(1));
         });
     }
 
     public function testLoadDataWithApiUser()
     {
+        $this->app->getModule('admin')->moduleMenus = ['admin' => $this->app->getModule('admin')->getMenu()];
         $this->app->getModule('admin')->dashboardLogDisplayApiUserData = 1;
         PermissionScope::run($this->app, function (PermissionScope $scope) {
             $this->createAdminLangFixture([]);
