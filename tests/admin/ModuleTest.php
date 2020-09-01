32db6ddddf0fc86e06a832164a15bf1eda64e4ec
Basil Suter <git@nadar.io>
2020-09-01 13:54:23 +0000

    add js message getter test

---
+++ tests/admin/ModuleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace admintests\admin;
+
+use admintests\AdminModelTestCase;
+use luya\admin\Module;
+
+class ModuleTest extends AdminModelTestCase
+{
+    public function testGetJsTranslations()
+    {
+        $module = new Module('admin');
+
+        $this->assertArrayHasKey(64, $module->getJsTranslationMessages());
+    }
+}
