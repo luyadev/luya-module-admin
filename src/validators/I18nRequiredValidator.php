1d21a80a852faeae3c7daaf45fb6456b1ae94568
Basil Suter <git@nadar.io>
2020-09-15 13:43:39 +0000

    add tests

--- src/validators/I18nRequiredValidator.php
+++ src/validators/I18nRequiredValidator.php
@@ -1,21 +1,37 @@
 <?php
 
-namespace luyas\admin\validators;
+namespace luya\admin\validators;
 
 use luya\admin\models\Lang;
+use luya\admin\Module;
 use luya\helpers\Json;
 use yii\validators\Validator;
 
 /**
  * Validate Required i18n Attributes.
  * 
- * Ensure the i18n attribute is correctly sent to the api and also checks if all language keys are given and NOT EMPTY.
+ * Ensure the i18n attribute is correctly and also checks if all language keys are given and the values for each language is not empty.
+ * 
+ * Example usage:
+ * 
+ * ```php
+ * [['title', 'location'], I18nRequiredValidator::class],
+ * ```
  * 
  * @author Basil Suter <git@nadar.io>
- * @since 1.0.0
+ * @since 3.6.0
  */
 class I18nRequiredValidator extends Validator
 {
+    public $invalidFormatMessage = "i18n_required_validator_invalid_format";
+
+    public $missingKeyMessage = "i18n_required_validator_missing_key";
+
+    public $emptyValueMessage = "i18n_required_validator_invalid_empty_value";
+
+    /**
+     * {@inheritDoc}
+     */
     public function validateAttribute($model, $attribute)
     {
         $array = $model->{$attribute};
@@ -25,16 +41,15 @@ class I18nRequiredValidator extends Validator
         }
 
         if (!is_array($array)) {
-            return $this->addError($model, $attribute, "The given attribute {attribute} must be type of array.", ['attribute' => $attribute]);
+            return $this->addError($model, $attribute, Module::t($this->invalidFormatMessage, ['attribute' => $attribute]));
         }
 
         foreach (Lang::find()->select(['short_code'])->asArray()->column() as $langShortCode) {
             if (!array_key_exists($langShortCode, $array)) {
-                $this->addError($model, $attribute, "The language key \"{lang}\" is missing and is required.",  ['lang' => $langShortCode]);
+                $this->addError($model, $attribute, Module::t($this->missingKeyMessage,  ['lang' => $langShortCode]));
             } elseif (empty($array[$langShortCode])) {
-                $this->addError($model, $attribute, "The value for language \"{lang}\" can not be empty.", ['lang' => $langShortCode]);
+                $this->addError($model, $attribute, Module::t($this->emptyValueMessage, ['lang' => $langShortCode]));
             }
         }
     }
-
 }
---
+++ tests/admin/validators/I18nRequiredValidatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace luya\admin\tests\admin\validators;
+
+use admintests\AdminModelTestCase;
+use luya\admin\validators\I18nRequiredValidator;
+use luya\base\DynamicModel;
+
+class I18nRequiredValidatorTest extends AdminModelTestCase
+{
+    public function testValidatorInvalidFormat()
+    {
+        $model = new DynamicModel(['i18n' => null]);
+        $validator = new I18nRequiredValidator();
+        $validator->validateAttribute($model, 'i18n');
+        $this->assertSame('The given attribute \"i18n\" must be type of array.', $model->getFirstError('i18n'));
+    }
+    public function testValidatorMissingLanguage()
+    {
+        $this->createAdminLangFixture([
+            1 => [
+                'id' => 1,
+                'short_code' => 'en',
+                'is_deleted' => 0,
+            ]
+        ]);
+        $model = new DynamicModel(['i18n' => []]);
+        $validator = new I18nRequiredValidator();
+        $validator->validateAttribute($model, 'i18n');
+        $this->assertSame('The language key \"en\" is missing and is required.', $model->getFirstError('i18n'));
+    }
+
+    public function testValidatorEmptyLanguage()
+    {
+        $this->createAdminLangFixture([
+            1 => [
+                'id' => 1,
+                'short_code' => 'en',
+                'is_deleted' => 0,
+            ]
+        ]);
+        $model = new DynamicModel(['i18n' => ['en' => '']]);
+        $validator = new I18nRequiredValidator();
+        $validator->validateAttribute($model, 'i18n');
+        $this->assertSame('The value for language \"en\" can not be empty.', $model->getFirstError('i18n'));
+    }
+}
