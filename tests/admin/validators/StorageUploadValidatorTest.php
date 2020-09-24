0ef3670e0b66e247935936010b2db03b173dc633
Basil <git@nadar.io>
2020-09-24 13:45:38 +0200

    storage upload for none ngrest (#565)
    * storage upload for none ngrest
    
    * handle multi upload
    
    * ensure error or empty files array
    
    * basic test
    
    * lock file
    
    * add changelog
    
    * upload test
    
    * add empty files test
    
    * add multi upload tests
    
    * remove globals info
    
    * add tests
    
    * change captured file source
    
    * php doc update

---
+++ tests/admin/validators/StorageUploadValidatorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace luya\admin\tests\admin\validators;
+
+use admintests\AdminModelTestCase;
+use luya\admin\validators\StorageUploadValidator;
+use luya\base\DynamicModel;
+use yii\web\UploadedFile;
+
+class StorageUploadValidatorTest extends AdminModelTestCase
+{
+    public function testSingleUploadValdiator()
+    {
+        UploadedFile::reset();
+        $validator = new StorageUploadValidator();
+        $model = new DynamicModel(['file_id' => 0]);
+        $this->createAdminStorageFileFixture();
+        $this->createAdminNgRestLogFixture();
+
+        $_FILES['DynamicModel[file_id]'] = [
+            'name' => 'MyFile.jpg',
+            'type' => 'image/jpeg',
+            'tmp_name' => dirname(__DIR__) . '/../data/image.jpg',
+            'error' => UPLOAD_ERR_OK,
+            'size' => 98174
+        ];
+        $response = $validator->validateAttribute($model, 'file_id');
+        
+        $validator->uploadToFiles($model, 'file_id');
+
+        $this->assertContains('myfile', $response);
+    }
+
+    public function testMultiUploadValidtor()
+    {
+        UploadedFile::reset();
+        $validator = new StorageUploadValidator();
+        $validator->multiple = true;
+
+        $model = new DynamicModel(['file_id' => 0]);
+        $this->createAdminStorageFileFixture();
+        $this->createAdminNgRestLogFixture();
+
+        $_FILES['DynamicModel[file_id]'] = [
+            'name' => 'MyFile.jpg',
+            'type' => 'image/jpeg',
+            'tmp_name' => dirname(__DIR__) . '/../data/image.jpg',
+            'error' => UPLOAD_ERR_OK,
+            'size' => 98174
+        ];
+        $response = $validator->validateAttribute($model, 'file_id');
+        
+        $this->assertContains('myfile', $model->file_id);
+        $this->assertContains('["', $model->file_id); // its a json!
+    }
+
+    public function testEmptyFilesArray()
+    {
+        UploadedFile::reset();
+        $_FILES = [];
+        $validator = new StorageUploadValidator();
+        $model = new DynamicModel(['file_id' => 0]);
+        $this->assertNull($validator->validateAttribute($model, 'file_id'));
+    }
+}
