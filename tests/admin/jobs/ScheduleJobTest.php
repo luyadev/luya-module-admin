8fae1e409ee7027ba8fcc7df2fd2308eb84abf20
Basil Suter <git@nadar.io>
2020-10-06 14:03:29 +0000

    add schedule job test

---
+++ tests/admin/jobs/ScheduleJobTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace luya\admin\tests\admin\jobs;
+
+use admintests\AdminConsoleSqLiteTestCase;
+use luya\admin\jobs\ScheduleJob;
+use luya\admin\models\Scheduler;
+use luya\admin\models\Tag;
+use luya\testsuite\fixtures\NgRestModelFixture;
+use luya\testsuite\traits\AdminDatabaseTableTrait;
+
+class ScheuldeJobTest extends AdminConsoleSqLiteTestCase
+{
+    use AdminDatabaseTableTrait;
+
+    public function testExectue()
+    {
+        $this->createAdminLangFixture();
+
+        new NgRestModelFixture([
+            'modelClass' => Tag::class,
+            'fixtureData' => [
+                1 => [
+                    'id' => 1,
+                    'name' => 'aaa',
+                    'translation' => '{"de":"aaa","en":"bbb"}',
+                ]
+            ]
+        ]);
+        new NgRestModelFixture([
+            'modelClass' => Scheduler::class,
+            'fixtureData' => [
+                1 => [
+                    'id' => 1,
+                    'model_class' => Tag::class,
+                    'primary_key' => 1,
+                    'target_attribute_name' => 'name',
+                    'new_attribute_value' => 'foobar',
+                ]
+            ]
+        ]);
+
+        $job = new ScheduleJob();
+        $job->schedulerId = 1;
+
+        $job->execute($this->app->adminqueue);
+
+        $tag = Tag::findOne(1);
+
+        $this->assertSame('foobar', $tag->name);
+    }
+}
