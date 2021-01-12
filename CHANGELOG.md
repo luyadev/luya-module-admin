8c2d11746777e2f8721892795d94057f5587f1d5
Basil <git@nadar.io>
2021-01-12 14:36:03 +0100

    list users groups (#610)
    * list users groups
    
    * changelog
    
    * add tests
    
    * yii2 dev-master

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ In order to read more about upgrading and BC breaks have a look at the [UPGRADE 
 
 ## 4.0.0
 
++ [#610](https://github.com/luyadev/luya-module-admin/pull/610) Added API endpoint to display all groups where the current loggedin user belongs to `admin/api-user-group/me`.
 + [#605](https://github.com/luyadev/luya-module-admin/pull/605) Add new LUYA Test Suite for wider PHP Testing Support. Added Tests for PHP 8.
 
 ## 3.9.0 (24. November 2020)
--- src/apis/GroupController.php
+++ src/apis/GroupController.php
@@ -2,7 +2,11 @@
 
 namespace luya\admin\apis;
 
+use luya\admin\components\Auth;
+use luya\admin\models\Group;
 use luya\admin\ngrest\base\Api;
+use Yii;
+use yii\data\ActiveDataProvider;
 
 /**
  * API to manage, create, udpate and delete all System Groups.
@@ -16,4 +20,29 @@ class GroupController extends Api
      * @var string The path to the group model.
      */
     public $modelClass = 'luya\admin\models\Group';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function actionPermissions()
+    {
+        return [
+            'me' => Auth::CAN_VIEW,
+        ];
+    }
+
+    /**
+     * Loggedin User Groups
+     * 
+     * Returns all groups where the current loggedin (me) belongs to.
+     *
+     * @return Group[]
+     * @since 4.0
+     */
+    public function actionMe()
+    {
+        return new ActiveDataProvider([
+            'query' => Yii::$app->adminuser->identity->getGroups(),
+        ]);
+    }
 }
