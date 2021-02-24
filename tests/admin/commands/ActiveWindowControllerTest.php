fc7794c9cf0f76be4ef2dcc637262da3482cbeb4
Basil <git@nadar.io>
2021-02-24 16:00:23 +0100

    echarts version 5.0 (#620)
    * echarts version 5.0
    
    * changelog
    
    * fix tests
    
    * fix newline

--- tests/admin/commands/ActiveWindowControllerTest.php
+++ tests/admin/commands/ActiveWindowControllerTest.php
@@ -31,7 +31,7 @@ use luya\admin\ngrest\base\ActiveWindow;
 class MeinTestActiveWindow extends ActiveWindow
 {
     /**
-     * @var string The name of the module where the ActiveWindow is located in order to finde the view path.
+     * @var string The name of the module where the ActiveWindow is located in order to find the view path.
      */
     public $module = '@cmsadmin';
 
