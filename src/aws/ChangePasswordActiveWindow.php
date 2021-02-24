0929b8f981f9738f60ef93ba6a5928efce981d73
Alex Schmid <schmid@netfant.ch>
2021-02-24 01:03:13 +0100

    Typo

--- src/aws/ChangePasswordActiveWindow.php
+++ src/aws/ChangePasswordActiveWindow.php
@@ -19,7 +19,7 @@ use luya\admin\ngrest\base\ActiveWindow;
 class ChangePasswordActiveWindow extends ActiveWindow
 {
     /**
-     * @var string The name of the module where the active windows is located in order to find the view path.
+     * @var string The name of the module where the ActiveWindow is located in order to find the view path.
      */
     public $module = 'admin';
     
