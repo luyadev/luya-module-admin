8530315479e27d6ab985b4fadc18b829cdfd6e45
Basil Suter <git@nadar.io>
2021-02-24 14:20:38 +0000

    fix tests

--- src/aws/ImageSelectCollectionActiveWindow.php
+++ src/aws/ImageSelectCollectionActiveWindow.php
@@ -67,7 +67,7 @@ class ImageSelectCollectionActiveWindow extends ActiveWindow
     public $sortIndexFieldName;
 
     /**
-     * @var string The name of the module where the active windows is located in order to finde the view path.
+     * @var string The name of the module where the active windows is located in order to find the view path.
      */
     public $module = 'admin';
 
