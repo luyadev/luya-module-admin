549bdf56017edf7cf90b9df156586e21ca3d8c4b
Alex Schmid <schmid@netfant.ch>
2021-02-24 01:04:44 +0100

    Typo

--- src/aws/FlowActiveWindow.php
+++ src/aws/FlowActiveWindow.php
@@ -38,7 +38,7 @@ use luya\admin\ngrest\base\ActiveWindow;
 class FlowActiveWindow extends ActiveWindow
 {
     /**
-     * @var string The name of the module where the active windows is located in order to finde the view path.
+     * @var string The name of the module where the ActiveWindow is located in order to find the view path.
      */
     public $module = '@admin';
     
