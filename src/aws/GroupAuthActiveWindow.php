ccb7e0eb5512e57975a4ffb14f55e9b00e9aac73
Alex Schmid <schmid@netfant.ch>
2021-02-24 01:05:54 +0100

    Typo

--- src/aws/GroupAuthActiveWindow.php
+++ src/aws/GroupAuthActiveWindow.php
@@ -17,7 +17,7 @@ use luya\helpers\ArrayHelper;
 class GroupAuthActiveWindow extends ActiveWindow
 {
     /**
-     * @var string The name of the module where the active windows is located in order to finde the view path.
+     * @var string The name of the module where the ActiveWindow is located in order to find the view path.
      */
     public $module = 'admin';
 
