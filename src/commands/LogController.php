b6dd9117b9453f959e8ea227824cb71cc61b6590
Basil Suter <git@nadar.io>
2021-03-09 07:49:19 +0000

    cs fix

--- src/commands/LogController.php
+++ src/commands/LogController.php
@@ -45,7 +45,7 @@ class LogController extends Command
      */
     private $_referenceTimestamp;
     /**
-     * Holds the old entries number found during age check 
+     * Holds the old entries number found during age check
      * @var integer
      */
     private $_oldRowsCount;
@@ -65,73 +65,76 @@ class LogController extends Command
      */
     public function actionCleanup($logTableName)
     {
-        if ( $this->_validateRows() && $this->_validateYears() && $this->_validateTables($logTableName)) {
+        if ($this->_validateRows() && $this->_validateYears() && $this->_validateTables($logTableName)) {
             // clean old log entries for each log tbale provided
             $this->_referenceTimestamp = strtotime(sprintf("-%s year", $this->years));
-            foreach ($this->_dbLogTables as $logTableName=>$timestampField){
+            foreach ($this->_dbLogTables as $logTableName=>$timestampField) {
                 // output header
-                $this->outputInfo( sprintf("\nChecking log table %s", $logTableName ));
-                $this->outputInfo( str_repeat("-", 80) );                
+                $this->outputInfo(sprintf("\nChecking log table %s", $logTableName));
+                $this->outputInfo(str_repeat("-", 80));
                 $this->_doClean($logTableName, $timestampField);
             }
         }
     } // END actionCleanup()
 
-    private function _doClean($logTableName, $timestampField){
-        if ( $this->_moreThanMinimumRowsFound($logTableName) && $this->_olderThanMiniumYearsFound($logTableName, $timestampField) && $this->_removalConfirmed($logTableName)) {
+    private function _doClean($logTableName, $timestampField)
+    {
+        if ($this->_moreThanMinimumRowsFound($logTableName) && $this->_olderThanMiniumYearsFound($logTableName, $timestampField) && $this->_removalConfirmed($logTableName)) {
             $removed = $this->dryRun ?  $this->_oldRowsCount : Yii::$app->db->createCommand()->delete("{{%$logTableName}}", "$timestampField < :timestampLimit", [
                 ':timestampLimit' => $this->_referenceTimestamp,
             ])->execute();
             if ($removed > 0) {
-               $this->outputSuccess(sprintf("%s entries removed", $removed));
-            }
-            else {
-               $this->outputInfo("No log entries renoved.");
+                $this->outputSuccess(sprintf("%s entries removed", $removed));
+            } else {
+                $this->outputInfo("No log entries renoved.");
             }
         }
     } // END _doClean()
 
-    private function _removalConfirmed($logTableName){
+    private function _removalConfirmed($logTableName)
+    {
         if ($this->interactive && !$this->confirm("Do you want to delete the extra entries from $logTableName table?")) {
             $this->outputError("Log entries clean-up aborted.");
             return false;
-        } 
+        }
         return true;
     }
     
-    private function _validateRows(){
+    private function _validateRows()
+    {
         // validate the minimum rows option
         $this->rows  = (int)$this->rows;
-        if ($this->rows < 0){
+        if ($this->rows < 0) {
             $this->outputError("Minimum rows to keep should be positive.");
             return false;
         }
         return true;
     }
 
-    private function _validateYears(){
+    private function _validateYears()
+    {
         // validate the years option
         $this->years = (int)$this->years;
-        if ($this->years< 0){
+        if ($this->years< 0) {
             $this->outputError("Minimum Years to keep should be positive.");
             return false;
         }
         return true;
     }
 
-    private function _validateTables($logTableName){
+    private function _validateTables($logTableName)
+    {
         // validate the log table names argument
-        if ( strtoupper($logTableName)!='ALL'){
+        if (strtoupper($logTableName)!='ALL') {
             // extarct the table names and check them towards the known list of log tables
             $logTableList = explode(',', $logTableName);
             $wrongTableNames = array_diff($logTableList, array_keys($this->_dbLogTables));
             if ($wrongTableNames) {
                 $this->outputInfo("Please specifiy a valid log table to clean among below list:");
                 $this->outputInfo("  - " . join("\n  - ", array_keys($this->_dbLogTables)));
-                $this->outputError(sprintf("Error. Invalid table name '%s'", join("', '", $wrongTableNames) ));
+                $this->outputError(sprintf("Error. Invalid table name '%s'", join("', '", $wrongTableNames)));
                 return false;
-            }
-            else {
+            } else {
                 // keep oonly provided table names
                 $this->_dbLogTables = array_flip(array_intersect(array_flip($this->_dbLogTables), $logTableList));
             }
@@ -139,10 +142,11 @@ class LogController extends Command
         return true;
     }
 
-    private function _moreThanMinimumRowsFound($logTableName){
-         // check entries count towards minimum threshold
+    private function _moreThanMinimumRowsFound($logTableName)
+    {
+        // check entries count towards minimum threshold
         $totalRowsCount = Yii::$app->db->createCommand("SELECT count(*) as count FROM {{%$logTableName}}")->queryScalar();
-        $this->output(sprintf("Total entries found : $totalRowsCount (minimum to keep %s)",  $this->rows));
+        $this->output(sprintf("Total entries found : $totalRowsCount (minimum to keep %s)", $this->rows));
         if ($totalRowsCount < $this->rows) {
             $this->outputInfo("Log entries do not execeed minium to keep.");
             return false;
@@ -150,13 +154,14 @@ class LogController extends Command
         return true;
     }
 
-    private function _olderThanMiniumYearsFound($logTableName, $timestampField){
+    private function _olderThanMiniumYearsFound($logTableName, $timestampField)
+    {
         //check entries age towards minimum years threshold
         $this->_oldRowsCount = Yii::$app->db->createCommand("SELECT count(*) as count FROM {{%$logTableName}} WHERE $timestampField < :timestampLimit", [
             ':timestampLimit' => $this->_referenceTimestamp,
         ])->queryScalar();
 
-        $this->output(sprintf("Total old entries : $this->_oldRowsCount (reference date %s)",  date('d-M-Y H:i:s', $this->_referenceTimestamp)));
+        $this->output(sprintf("Total old entries : $this->_oldRowsCount (reference date %s)", date('d-M-Y H:i:s', $this->_referenceTimestamp)));
 
         if ($this->_oldRowsCount == 0) {
             $this->outputInfo("Log entries are  not old enough to delete.");
@@ -164,5 +169,4 @@ class LogController extends Command
         }
         return true;
     }
-
 } // END LogController class
