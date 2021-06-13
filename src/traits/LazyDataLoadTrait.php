901f2e44d2382c41cdf9595472827b77558c189d
boehsermoe <boehsermoe@me.com>
2021-06-13 09:33:30 +0200

    phpdocs for LazyDataLoadTrait

--- src/traits/LazyDataLoadTrait.php
+++ src/traits/LazyDataLoadTrait.php
@@ -2,20 +2,48 @@
 
 namespace luya\admin\traits;
 
+/**
+ * Trait to enable lazy load for NgRest plugins.
+ *
+ * For a example for CheckboxList:
+ *
+ * ```php
+ * public function ngRestAttributeTypes()
+ * {
+ *     return [
+ *         'genres' => ['checkboxList', 'data' => function () {
+ *               return new Query()->all();
+ *          }],
+ *     ];
+ * }
+ * ```
+ *
+ * @author Bennet Klarhölter <boehsermoe@me.com>
+ * @since 4.0.0
+ */
 trait LazyDataLoadTrait
 {
     /**
-     * @param array|\Closure $data
-     * @return array|\Traversable
+     * If the given data is a closure it will call it and return the result of the function.
+     * Function will only execute if the data are really need to load.
+     *
+     * If the given data is no closure it will return the directly data.
+     *
+     * ```
+     * $this->lazyLoadData(function () {
+     *      return Query::find()->all();
+     * });
+     * ```
+     *
+     * @param mixed|\Closure $data
+     * @return mixed|\Traversable
      */
-    protected function loadData($data)
+    protected function lazyLoadData($data)
     {
         if (is_callable($data)) {
             return call_user_func($data);
-        } elseif (is_array($data) || $data instanceof \Traversable) {
-            return $data;
         }
         
-        return [];
+        return $data;
     }
 }
