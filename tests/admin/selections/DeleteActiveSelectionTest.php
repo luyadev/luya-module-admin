485897862d302640e21e8ceac7ee9a7a8bf69fb9
Basil Suter <git@nadar.io>
2021-06-17 09:59:01 +0000

    tests

--- tests/admin/selections/DeleteActiveSelectionTest.php
+++ tests/admin/selections/DeleteActiveSelectionTest.php
@@ -3,6 +3,7 @@
 namespace luya\admin\tests\admin\selections;
 
 use admintests\AdminModelTestCase;
+use luya\admin\ngrest\base\ActiveSelection;
 use luya\admin\selections\DeleteActiveSelection;
 
 class DeleteActiveSelectionTest extends AdminModelTestCase
@@ -14,5 +15,30 @@ class DeleteActiveSelectionTest extends AdminModelTestCase
 
         $this->assertTrue($r['success']);
         $this->assertFalse($obj->sendError('foo')['success']);
+    }
+
+    public function testExternalHandle()
+    {
+        $obj = new ActiveSelection();
+        $obj->action = function(array $items, ActiveSelection $context) {
+            return true;
+        };
+        $r = $obj->handle([]);
+
+        $this->assertTrue($r['success']);
+
+        $obj->action = function(array $items, ActiveSelection $context) {
+            return false;
+        };
+        $r = $obj->handle([]);
+
+        $this->assertTrue($r['error']);
+
+        $obj->action = function(array $items, ActiveSelection $context) {
+            return ['foo' => 'bar'];
+        };
+        $r = $obj->handle([]);
+
+        $this->assertSame(['foo' => 'bar'], $r);
     }
 }
