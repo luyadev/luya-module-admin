f675018feda014dcd10b444431403f468ee2a817
Basil Suter <git@nadar.io>
2021-08-10 11:13:51 +0000

    Removed old command closes #652

--- src/commands/StorageController.php
+++ src/commands/StorageController.php
@@ -18,48 +18,6 @@ use luya\console\Command;
 class StorageController extends Command
 {
     /**
-     * Delete orphaned files, but requires user confirmation to ensure delete process.
-     */
-    public function actionCleanup()
-    {
-        $fileList = StorageImporter::getOrphanedFileList();
-
-        if ($fileList === false) {
-            return $this->outputError("Could not find the storage folder to clean up.");
-        }
-        
-        if (count($fileList) > 0) {
-            foreach ($fileList as $fileName) {
-                $this->output($fileName);
-            }
-            $this->outputInfo(count($fileList) . " files to remove.");
-        }
-
-        if (count($fileList) !== 0) {
-            $success = true;
-            if ($this->confirm("Do you want to delete " . count($fileList) . " files which are not referenced in the database any more? (can not be undon, maybe create a backup first!)")) {
-                foreach ($fileList as $file) {
-                    if (is_file($file) && @unlink($file)) {
-                        $this->outputSuccess($file . " successful deleted.");
-                    } elseif (is_file($file)) {
-                        $this->outputError($file . " could not be deleted!");
-                        $success = false;
-                    } else {
-                        $this->outputError($file . " could not be found!");
-                        $success = false;
-                    }
-                }
-            }
-            if ($success) {
-                return $this->outputSuccess(count($fileList) . " files successful deleted.");
-            }
-            return $this->outputError("Cleanup could not be completed. Please look into error above.");
-        }
-        
-        return $this->outputSuccess("No orphaned files found.");
-    }
-    
-    /**
      * Create all thumbnails for filemanager preview. Otherwhise they are created on request load.
      */
     public function actionProcessThumbnails()
