b84fc5997c4ef99ad2542457e18a5df2052f17bc
Basil Suter <git@nadar.io>
2022-04-13 08:24:09 +0000

    fix bug for exporter with more then 100 rows

--- src/ngrest/base/Api.php
+++ src/ngrest/base/Api.php
@@ -869,13 +869,15 @@ class Api extends RestActiveController
     {
         Yii::$app->formatter->nullDisplay = '';
         $data = [];
+        $rowId = 0;
         foreach ($query->batch() as $batch) {
-            foreach ($batch as $key => $model) {
+            foreach ($batch as $model) {
+                $rowId++;
                 foreach ($formatter as $formatterAttribute => $formatAs) {
                     if (is_callable($formatAs)) {
-                        $data[$key][$formatterAttribute] = call_user_func($formatAs, $model);
+                        $data[$rowId][$formatterAttribute] = call_user_func($formatAs, $model);
                     } else {
-                        $data[$key][$formatterAttribute] = Yii::$app->formatter->format($model[$formatterAttribute], $formatAs);
+                        $data[$rowId][$formatterAttribute] = Yii::$app->formatter->format($model[$formatterAttribute], $formatAs);
                     }
                 }
             }
